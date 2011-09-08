2c9527b736e4cd33862f0823df7f54c0399f6f07
David Hughes <dah2117@columbia.edu>
2011-09-08 11:12:54 -0400

    Added a few lines to shell.c, learned about forking

--- shell/shell.c
+++ shell/shell.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	pid_t pid;
+
+	printf("Starting Simple Shell\n");
+
+	pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "fork failed\n");
+		return -1;
+	} else if (pid == 0) {
+		printf("This is the child process %d\n", pid);
+	} else {
+		printf("This is the parent process %d\n", pid);
+	}
 	return EXIT_SUCCESS;
 }
