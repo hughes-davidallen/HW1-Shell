4e8135f165dcb57daf089fa00c0b625784b76396
David Hughes <dah2117@columbia.edu>
2011-09-10 01:32:59 -0400

    Pretty much done with part 1

--- shell/shell.c
+++ shell/shell.c
@@ -11,44 +11,54 @@
 int main(int argc, char *argv[])
 {
 	pid_t pid;
-	char *args[MAX_ARGS + 1];
+	const char *prompt = "sh>";
+	char *line; /* location for entire input line */
+	char *args[MAX_ARGS + 1]; /* individual arguments */
 	int i; /* counters for parsing the arguments */
-	char *line;
 	size_t len;
 
 	memset(args, 0, sizeof(args));
 
-	printf("Starting Simple Shell\n>");
+	printf("Starting Simple Shell %s\n%s", argv[0], prompt);
 
-	getline(&line, &len, stdin);
+	while (1) {
+		getline(&line, &len, stdin);
 
-	args[0] = strtok(line, " \t");
-	for (i = 1; i <= MAX_ARGS; i++) {
-		args[i] = strtok(NULL, " \t\n");
-		if (args[i] == NULL)
+		args[0] = strtok(line, " \t\n");
+		if (strcmp(args[0], "exit") == 0) {
+			free(line);
+			printf("sh: Goodbye\n\n");
 			break;
-	}
+		}
 
-	pid = fork();
-	if (pid < 0) {
-		fprintf(stderr, "fork failed\n");
-		return EXIT_FAILURE;
-	} else if (pid == 0) {
-	/*	printf("This is the child process %d\n", pid);*/
-		execvp(args[0], args);
+		for (i = 1; i <= MAX_ARGS; i++) {
+			args[i] = strtok(NULL, " \t\n");
+			if (args[i] == NULL)
+				break;
+		}
 
-		/*
-		 * If execution reaches this point, the
-		 * exec system call has failed
-		 */
-		fprintf(stderr, "sh: unknown command: %s\n", args[0]);
-		exit(1);
-	} else {
-		int rVal;
-		while (pid != wait(&rVal))
-			;
-/*		printf("This is the parent process %d - %d\n", 
-			pid, rVal); */
+		pid = fork();
+		if (pid < 0) {
+			fprintf(stderr, "fork failed\n");
+			return EXIT_FAILURE;
+		} else if (pid == 0) {
+		/*	printf("This is the child process %d\n", pid);*/
+			execvp(args[0], args);
+
+			/*
+		 	* If execution reaches this point, the
+		 	* exec system call has failed
+		 	*/
+			fprintf(stderr, "sh: unknown command: %s\n", args[0]);
+			exit(1);
+		} else {
+			int rVal;
+			while (pid != wait(&rVal))
+				;
+			printf("%s", prompt);
+/*			printf("This is the parent process %d - %d\n", 
+				pid, rVal); */
+		}
 	}
 	return EXIT_SUCCESS;
 }
