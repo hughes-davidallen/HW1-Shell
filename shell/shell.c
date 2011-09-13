3a988ec66c35464bfeb2b3cdf9018f69a0fd6dd6
David Hughes <dah2117@columbia.edu>
2011-09-13 11:23:18 -0400

    Added support for the cd command and the associated error checking

--- shell/shell.c
+++ shell/shell.c
@@ -5,13 +5,17 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define MAX_ARGS 32
+
+int cd(char *path);
 
 int main(int argc, char *argv[])
 {
 	pid_t pid;
 	const char *prompt = "sh>";
+	const char *delim = " \t\n";
 	char *line; /* location for entire input line */
 	char *args[MAX_ARGS + 1]; /* individual arguments */
 	int i; /* counters for parsing the arguments */
@@ -19,22 +23,36 @@ int main(int argc, char *argv[])
 
 	memset(args, 0, sizeof(args));
 
-	printf("Starting Simple Shell %s\n%s", argv[0], prompt);
+	printf("Starting Simple Shell %s\n", argv[0]);
 
-	while (1) {
-		getline(&line, &len, stdin);
+	while (1) { /* keep prompting until user 'exit's */
+		printf("%s", prompt);
 
-		args[0] = strtok(line, " \t\n");
+		getline(&line, &len, stdin); /* read all input at once */
+
+		args[0] = strtok(line, delim);
+		for (i = 1; i <= MAX_ARGS; i++) {
+			args[i] = strtok(NULL, delim);
+			if (args[i] == NULL)
+				break;
+		}
+
+		/* Handle built-in commands */
 		if (strcmp(args[0], "exit") == 0) {
 			free(line);
 			printf("sh: Goodbye\n\n");
 			break;
-		}
-
-		for (i = 1; i <= MAX_ARGS; i++) {
-			args[i] = strtok(NULL, " \t\n");
-			if (args[i] == NULL)
-				break;
+		} else if (strcmp(args[0], "cd") == 0) {
+			cd(args[1]);
+			continue; /* don't fork a new process */
+		} else if (strcmp(args[0], "pushd") == 0) {
+			continue;
+		} else if (strcmp(args[0], "popd") == 0) {
+			continue;
+		} else if (strcmp(args[0], "dirs") == 0) {
+			continue;
+		} else if (strcmp(args[0], "path") == 0) {
+			continue;
 		}
 
 		pid = fork();
@@ -42,23 +60,63 @@ int main(int argc, char *argv[])
 			fprintf(stderr, "fork failed\n");
 			return EXIT_FAILURE;
 		} else if (pid == 0) {
-		/*	printf("This is the child process %d\n", pid);*/
+		/*	printf("This is the child process %-d\n", pid);*/
 			execvp(args[0], args);
 
 			/*
-		 	* If execution reaches this point, the
-		 	* exec system call has failed
-		 	*/
+		 	 * If execution reaches this point, the
+		 	 * exec system call has failed
+		 	 */
 			fprintf(stderr, "sh: unknown command: %s\n", args[0]);
 			exit(1);
 		} else {
 			int rVal;
 			while (pid != wait(&rVal))
 				;
-			printf("%s", prompt);
-/*			printf("This is the parent process %d - %d\n", 
-				pid, rVal); */
 		}
 	}
 	return EXIT_SUCCESS;
+}
+
+/*
+ * Handles the cd command by makinga chdir system call.  Also interprets
+ * errno in the event of a system call failure.
+ */
+int cd(char *path)
+{
+	int status;
+	status = chdir(path); /* make a system call */
+	if (status != 0) {
+		fprintf(stderr, "sh: could not change directory\n");
+		switch(errno) {
+		case EACCES:
+			fprintf(stderr, "\tPermission denied\n");
+			break;
+		case EFAULT:
+			fprintf(stderr, "\tPath outside memory space\n");
+			break;
+		case EIO:
+			fprintf(stderr, "\tIO error\n");
+			break;
+		case ELOOP:
+			fprintf(stderr, "\tToo many symbolic links\n");
+			break;
+		case ENAMETOOLONG:
+			fprintf(stderr, "\tPath name too long\n");
+			break;
+		case ENOENT:
+			fprintf(stderr, "\tDirectory does not exist\n");
+			break;
+		case ENOMEM:
+			fprintf(stderr, "\tInsufficient kernal memory\n");
+			break;
+		case ENOTDIR:
+			fprintf(stderr, "\tNot a directory\n");
+			break;
+		default:
+			break;
+		}
+	}
+
+	return status;
 }
