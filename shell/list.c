86ffc86899227eff47cc71109492ce475af4ac94
David Hughes <dah2117@columbia.edu>
2011-09-14 17:47:13 -0400

    Added a linked list to keep track of the path

---
+++ shell/list.c
@@ -0,0 +1,102 @@
+/* Represents a singly linked list of strings */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "list.h"
+
+/* initialize an empty linked list */
+struct list *list_init(void)
+{
+	struct list *l;
+
+	l = (struct list *) malloc(sizeof(struct list));
+	l->head = NULL;
+	l->tail = NULL;
+	return l;
+}
+
+/* free all data and links and then the list itself */
+void list_free(struct list *l)
+{
+	struct link *cursor;
+	struct link *old_cursor;
+
+	cursor = l->head;
+	while (cursor != NULL) {
+		free(cursor->data);
+		old_cursor = cursor;
+		cursor = cursor->next;
+		free(old_cursor);
+	}
+	free(l);
+}
+
+/* copy a word and append it to the end of the list */
+void list_add(struct list *l, char *word)
+{
+	struct link *lin;
+
+	lin = (struct link *) malloc(sizeof(struct link));
+	lin->next = NULL;
+	lin->data = strdup(word);
+
+	if (l->tail == NULL) { /* special case: empty list */
+		l->head = lin;
+		l->tail = lin;
+	} else {
+		l->tail->next = lin;
+		l->tail = lin;
+	}
+}
+
+/*
+ * search the list for a word and remove it if found
+ * return 1 if found and removed and 0 if not found
+ */
+int list_remove(struct list *l, char *word)
+{
+	struct link *cursor;
+	struct link *prev;
+
+	cursor = l->head;
+	prev = NULL;
+	while (cursor != NULL) {
+		if (strcmp(word, cursor->data) == 0)
+			break;
+
+		prev = cursor;
+		cursor = cursor->next;
+	}
+
+	if (cursor == NULL) { /* word not found in list */
+		return 0;
+	} else if (prev == NULL) { /* word is first item in list */
+		l->head = cursor->next;
+		free(cursor->data);
+		free(cursor);
+	} else if (cursor->next == NULL) { /*word is last in list */
+		l->tail = prev;
+		prev->next = NULL;
+		free(cursor->data);
+		free(cursor);
+	} else { /* word is found but not front of list */
+		prev->next = cursor->next;
+		free(cursor->data);
+		free(cursor);
+	}
+
+	return 1;
+}
+
+/* print the contents of the list */
+void list_print(struct list *l)
+{
+	struct link *cursor;
+
+	cursor = l->head;
+	while (cursor != NULL) {
+		printf("%s\n", cursor->data);
+		cursor = cursor->next;
+	}
+}
---
+++ shell/list.h
@@ -0,0 +1,20 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+struct link {
+	struct link *next;
+	char *data;
+};
+
+struct list {
+	struct link *head;
+	struct link *tail;
+};
+
+struct list *list_init(void);
+void list_free(struct list *l);
+void list_add(struct list *l, char *word);
+int list_remove(struct list *l, char *word);
+void list_print(struct list *l);
+
+#endif
