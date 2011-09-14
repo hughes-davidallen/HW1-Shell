682215ef2ee92184a465876a922acca200efd3f1
David Hughes <dah2117@columbia.edu>
2011-09-13 21:27:44 -0400

    Finished steps 1 through 3 and outlined the path stuff

--- shell/shell.c
+++ shell/shell.c
@@ -9,27 +9,35 @@
 
 #define MAX_ARGS 32
 
+void setup(void);
+void cleanup(void);
 int cd(char *path);
+int pushd(char *path);
+int popd(void);
+void dirs(void);
+void chuck(char *symbol, char *dir);
 
 int main(int argc, char *argv[])
 {
-	pid_t pid;
-	const char *prompt = "sh>";
-	const char *delim = " \t\n";
+	pid_t pid; /* will be result of the fork() system call */
+	const char *prompt = "sh>"; /* displays when shell is ready */
+	const char *delim = " \t\n"; /* delimeters for argument parsing */
 	char *line; /* location for entire input line */
 	char *args[MAX_ARGS + 1]; /* individual arguments */
-	int i; /* counters for parsing the arguments */
-	size_t len;
+	int i; /* counter for parsing the arguments */
+	size_t len; /* required by getline() function */
 
 	memset(args, 0, sizeof(args));
 
 	printf("Starting Simple Shell %s\n", argv[0]);
+	setup(); /* set up the environment */
 
 	while (1) { /* keep prompting until user 'exit's */
 		printf("%s", prompt);
 
 		getline(&line, &len, stdin); /* read all input at once */
 
+		/* put all individual words into an array */
 		args[0] = strtok(line, delim);
 		for (i = 1; i <= MAX_ARGS; i++) {
 			args[i] = strtok(NULL, delim);
@@ -37,30 +45,34 @@ int main(int argc, char *argv[])
 				break;
 		}
 
-		/* Handle built-in commands */
+		/* Handle built-in commands without forking */
 		if (strcmp(args[0], "exit") == 0) {
 			free(line);
 			printf("sh: Goodbye\n\n");
 			break;
 		} else if (strcmp(args[0], "cd") == 0) {
 			cd(args[1]);
-			continue; /* don't fork a new process */
+			continue;
 		} else if (strcmp(args[0], "pushd") == 0) {
+			pushd(args[1]);
 			continue;
 		} else if (strcmp(args[0], "popd") == 0) {
+			popd();
 			continue;
 		} else if (strcmp(args[0], "dirs") == 0) {
+			dirs();
 			continue;
 		} else if (strcmp(args[0], "path") == 0) {
+			chuck(args[1], args[2]);
 			continue;
 		}
 
+		/* handle non built-ins by forking a new process */
 		pid = fork();
 		if (pid < 0) {
-			fprintf(stderr, "fork failed\n");
+			fprintf(stderr, "sh: fork failed\n");
 			return EXIT_FAILURE;
 		} else if (pid == 0) {
-		/*	printf("This is the child process %-d\n", pid);*/
 			execvp(args[0], args);
 
 			/*
@@ -75,11 +87,59 @@ int main(int argc, char *argv[])
 				;
 		}
 	}
+	cleanup();
+
 	return EXIT_SUCCESS;
 }
 
 /*
- * Handles the cd command by makinga chdir system call.  Also interprets
+ * This section of the code represents the guts of the shell.  This is
+ * where all of the built-in commands are handled.
+ */
+
+#include "stack.h"
+
+#define MAX_PATH_LENGTH 100
+
+struct stack *stack; /* used to store old directory locations */
+char *path = "PATH=";
+char *pwd;
+
+/*
+ * setup
+ */
+void setup(void)
+{
+	char **env; /* used to help clobber the PATH variable */
+
+	/*
+	 * Clobber the old PATH environment variable.  environ represents
+	 * the implicit environment variables argument used by exec
+	 */
+	env = environ;
+	while(*env != NULL) {
+		if (strncmp(*env, "PATH", 4) == 0) {
+			*env = path;
+		}
+		env++;
+	}
+
+	stack = stack_init();
+
+	pwd = getcwd(NULL, MAX_PATH_LENGTH);
+}
+
+/*
+ * cleanup
+ */
+void cleanup(void)
+{
+	stack_free(stack);
+	free(pwd);
+}
+
+/*
+ * Handles the cd command by making a chdir system call.  Also interprets
  * errno in the event of a system call failure.
  */
 int cd(char *path)
@@ -118,5 +178,57 @@ int cd(char *path)
 		}
 	}
 
+	if ((pwd = getcwd(NULL, MAX_PATH_LENGTH)) == NULL) {
+		fprintf(stderr, "sh: can't get current directory\n");
+	}
+
 	return status;
+}
+
+/*
+ * pushd
+ */
+int pushd(char *path)
+{
+	push(stack, pwd);
+	return cd(path);
+}
+
+/*
+ * popd
+ */
+int popd(void)
+{
+	if (stack->top == NULL) {
+		fprintf(stderr, "sh: Directory stack is empty\n");
+		return -1;
+	}
+
+	printf("%s\n", peek(stack));
+
+	return cd(pop(stack));
+}
+
+/*
+ * dirs
+ */
+void dirs(void)
+{
+	stack_print(stack);
+}
+
+/*
+ * chuck - just until I can rename the path handler
+ */
+void chuck(char *symbol, char *dir)
+{
+	if (symbol == NULL) {
+		printf("%s\n", path + 5);
+	} else if (*symbol == '+' && strlen(symbol) == 1) {
+		
+	} else if (*symbol == '-' && strlen(symbol) == 1) {
+		
+	} else {
+
+	}
 }
